cd216b344ca1fceb8f657d05d779c9e32706ff18
Muazzam <muazzamaqeel@outlook.com>
2024-12-14 15:18:48 +0100

    Updates

--- source_code/InfiniteRush/src/Main.java
+++ source_code/InfiniteRush/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Racing racing = new Racing();
+        // Specify the number of players for the multiplayer game
+        RacingMultiplayer racing = new RacingMultiplayer(2); // Example: 2 players
         racing.startGame();
     }
 }
